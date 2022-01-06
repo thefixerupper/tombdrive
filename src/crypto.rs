dae33e65239330416b6bb5a433ed07e0e2071c4d
Martin Furman <68301881+thefixerupper@users.noreply.github.com>
2022-01-05 20:52:01 -0800

    Adding passphrase support into the `CryptoReader`

--- src/crypto.rs
+++ src/crypto.rs
@@ -28,12 +28,18 @@
 use std::io::{ self, BufReader, BufWriter, ErrorKind,
                Read, Seek, SeekFrom, Write };
 
+use hmac::Hmac;
+use pbkdf2::pbkdf2;
+use sha2::Sha256;
 
 /// The number of bytes in the [`MAGIC_NUMBER`]
-const MAGIC_NUMBER_LEN: usize = 8;
+const MAGIC_NUMBER_LEN: usize = 4;
 
 /// Magic bytes that are added to the beginning of every encrypted stream
-const MAGIC_NUMBER: &[u8; MAGIC_NUMBER_LEN] = b"TombDrv:";
+const MAGIC_NUMBER: &[u8; MAGIC_NUMBER_LEN] = b"Tomb";
+
+/// The number of bytes used to quickly check for invalid passphrases
+const PASSPHRASE_HASH_LEN: usize = 4;
 
 /// The number of bytes in the counter attribute
 const COUNTER_LEN: usize = 16;
@@ -49,6 +55,12 @@ const SIZE_LEN: usize = 8;
 const HEADER_LEN: u64 = (MAGIC_NUMBER_LEN + COUNTER_LEN +
                          SALT_LEN + SIZE_LEN) as u64;
 
+/// The number of bytes in a key
+const KEY_LEN: usize = 32;
+
+/// The number of derivation rounds
+const KEY_ROUNDS: u32 = 1_000_000;
+
 
 /// After the [`MAGIC_NUMBER`], each non-zero-size encrypted stream holds
 /// a couple of numerical attributes encrypted as big-endian bytes
@@ -56,6 +68,8 @@ const HEADER_LEN: u64 = (MAGIC_NUMBER_LEN + COUNTER_LEN +
 struct Attributes {
     /// The counter used to encrypt the first block of data
     counter: u128,
+    /// The seed for passphrase check
+    passphrase_hash: [u8; PASSPHRASE_HASH_LEN],
     /// The salt used for key derivation
     salt: u128,
     /// The size of the plaintext data (i.e. unencrypted stream)
@@ -69,20 +83,22 @@ struct Attributes {
 ///
 #[derive(Debug)]
 pub struct CryptoReader<T: Read + Seek> {
-    reader: BufReader<T>,
     attributes: Attributes,
+    key: [u8; KEY_LEN],
+    reader: BufReader<T>,
     total_len: u64,
 }
 
 impl<T: Read + Seek> CryptoReader<T> {
     ///
-    /// Create a new buffered [`CryptoReader<T>`].
+    /// Create a new buffered [`CryptoReader<T>`] that decrypts an encrypted
+    /// file using `passphrase`.
     ///
     /// This function will return a [`io::Error`] if the `stream` is empty or
     /// if the `stream` does not look like it was encrypted
     /// by [`CryptoWriter<T>`].
     ///
-    pub fn new(mut stream: T) -> io::Result<CryptoReader<T>> {
+    pub fn new(mut stream: T, passphrase: &str) -> io::Result<CryptoReader<T>> {
         let total_len = stream.seek(SeekFrom::End(0))?;
         if total_len == 0 {
             return Err(io::Error::new(ErrorKind::Other, "Empty stream"));
@@ -93,12 +109,14 @@ impl<T: Read + Seek> CryptoReader<T> {
         let mut reader = BufReader::new(stream);
 
         let mut magic_number = [0u8; MAGIC_NUMBER_LEN];
+        let mut passphrase_hash = [0u8; PASSPHRASE_HASH_LEN];
         let mut size_bytes = [0u8; SIZE_LEN];
         let mut salt_bytes = [0u8; SALT_LEN];
         let mut counter_bytes = [0u8; COUNTER_LEN];
 
         reader.seek(SeekFrom::Start(0))?;
         reader.read_exact(&mut magic_number)?;
+        reader.read_exact(&mut passphrase_hash)?;
         reader.read_exact(&mut size_bytes)?;
         reader.read_exact(&mut salt_bytes)?;
         reader.read_exact(&mut counter_bytes)?;
@@ -109,13 +127,25 @@ impl<T: Read + Seek> CryptoReader<T> {
 
         let size = u64::from_be_bytes(size_bytes);
         if size + HEADER_LEN != total_len {
-            return Err(io::Error::new(ErrorKind::Other, "Wrong stream size"));
+            return Err(io::Error::new(ErrorKind::InvalidData, "Wrong stream size"));
         }
 
         let salt = u128::from_be_bytes(salt_bytes);
         let counter = u128::from_be_bytes(counter_bytes);
 
-        let attributes = Attributes { counter, salt, size };
-        Ok(CryptoReader { reader, attributes, total_len })
+        let attributes = Attributes { counter, passphrase_hash, salt, size };
+
+        let mut key = [0u8; KEY_LEN];
+        pbkdf2::<Hmac<Sha256>>(passphrase.as_bytes(), &salt_bytes,
+                               KEY_ROUNDS, &mut key);
+
+        let mut passphrase_hash_check = [0u8; PASSPHRASE_HASH_LEN];
+        pbkdf2::<Hmac<Sha256>>(passphrase.as_bytes(), &salt_bytes,
+                               KEY_ROUNDS, &mut passphrase_hash_check);
+        if passphrase_hash != passphrase_hash_check {
+            return Err(io::Error::new(ErrorKind::Other, "Wrong passphrase"));
+        }
+
+        Ok(CryptoReader { attributes, key, reader, total_len })
     }
 }
