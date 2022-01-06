90beac3d7cb6bd66119bd0108ff8a7ba3bd9ef65
Martin Furman <68301881+thefixerupper@users.noreply.github.com>
2022-01-05 23:55:18 -0800

    Renaming `CryptoReader` to `DecryptionReader`

--- src/crypto.rs
+++ src/crypto.rs
@@ -15,18 +15,17 @@
 // limitations under the License.
 
 //!
-//! Provides [`CryptoReader<T>`] and [`CryptoWriter<T>`] types used to access
-//! encrypted streams (i.e. encrypted files).
+//! Provides [`EncryptionReader<T>`] and [`DecryptionReader<T>`] types used
+//! to translate from and to the encrypted container format.
 //!
-//! These types behave akin to [`BufReader<R>`] and [`BufWriter<W>`]
-//! (and actually use the two for buffered access).
+//! These types behave akin to [`BufReader<R>`] (and actually use
+//! `BufReader<R>` internally more efficient access management).
 //!
-//! They transparently read-and-decrypt/encrypt-and-write data (as well as any
-//! associated attributes) from/to the backing type.
+//! They transparently encrypt/decrypt data (as well as any associated
+//! attributes) from the backing stream.
 //!
 
-use std::io::{ self, BufReader, BufWriter, ErrorKind,
-               Read, Seek, SeekFrom, Write };
+use std::io::{ self, BufReader, ErrorKind, Read, Seek, SeekFrom };
 
 use hmac::Hmac;
 use pbkdf2::pbkdf2;
@@ -38,7 +37,8 @@ const MAGIC_NUMBER_LEN: usize = 4;
 /// Magic bytes that are added to the beginning of every encrypted stream
 const MAGIC_NUMBER: &[u8; MAGIC_NUMBER_LEN] = b"Tomb";
 
-/// The number of bytes used to quickly check for invalid passphrases
+/// The number of bytes used by [`DecryptionReader<T>`] to quickly check
+/// for invalid passphrases
 const PASSPHRASE_HASH_LEN: usize = 4;
 
 /// The number of bytes in the counter attribute
@@ -47,7 +47,7 @@ const COUNTER_LEN: usize = 16;
 /// The number of bytes in the salt attribute
 const SALT_LEN: usize = 16;
 
-/// The number of bytes representing the size of a plaintext stream
+/// The number of bytes representing the size of the plaintext
 const SIZE_LEN: usize = 8;
 
 /// The number of bytes in the entire header (magic number + attributes)
@@ -63,13 +63,13 @@ const KEY_ROUNDS: u32 = 1_000_000;
 
 
 /// After the [`MAGIC_NUMBER`], each non-zero-size encrypted stream holds
-/// a couple of numerical attributes encrypted as big-endian bytes
+/// a couple of numerical attributes encrypted as big-endian bytes.
+/// [`Attributes`] hold the ones that we need to keep around while
+/// operating on the stream.
 #[derive(Debug)]
-struct Attributes {
+pub struct Attributes {
     /// The counter used to encrypt the first block of data
     counter: u128,
-    /// The seed for passphrase check
-    passphrase_hash: [u8; PASSPHRASE_HASH_LEN],
     /// The salt used for key derivation
     salt: u128,
     /// The size of the plaintext data (i.e. unencrypted stream)
@@ -78,27 +78,26 @@ struct Attributes {
 
 
 ///
-/// The [`CryptoReader<T>`] creates a translation layer where the encrypted
+/// The [`DecryptionReader<T>`] creates a translation layer where the encrypted
 /// data from the backing stream gets decrypted on the fly as it is being read.
 ///
 #[derive(Debug)]
-pub struct CryptoReader<T: Read + Seek> {
+pub struct DecryptionReader<T: Read + Seek> {
     attributes: Attributes,
     key: [u8; KEY_LEN],
     reader: BufReader<T>,
-    total_len: u64,
 }
 
-impl<T: Read + Seek> CryptoReader<T> {
+impl<T: Read + Seek> DecryptionReader<T> {
     ///
-    /// Create a new buffered [`CryptoReader<T>`] that decrypts an encrypted
-    /// file using `passphrase`.
+    /// Create a new buffered [`DecryptionReader<T>`] that decrypts
+    /// an encrypted stream using `passphrase`.
     ///
-    /// This function will return a [`io::Error`] if the `stream` is empty or
-    /// if the `stream` does not look like it was encrypted
-    /// by [`CryptoWriter<T>`].
+    /// This function will return an [`io::Error`] if the `stream` is empty or
+    /// if the `stream` does not look like it was encrypted using
+    /// [`EncryptionReader<T>`].
     ///
-    pub fn new(mut stream: T, passphrase: &str) -> io::Result<CryptoReader<T>> {
+    pub fn new(mut stream: T, passphrase: &str) -> io::Result<DecryptionReader<T>> {
         let total_len = stream.seek(SeekFrom::End(0))?;
         if total_len == 0 {
             return Err(io::Error::new(ErrorKind::Other, "Empty stream"));
@@ -133,7 +132,7 @@ impl<T: Read + Seek> CryptoReader<T> {
         let salt = u128::from_be_bytes(salt_bytes);
         let counter = u128::from_be_bytes(counter_bytes);
 
-        let attributes = Attributes { counter, passphrase_hash, salt, size };
+        let attributes = Attributes { counter, salt, size };
 
         let mut key = [0u8; KEY_LEN];
         pbkdf2::<Hmac<Sha256>>(passphrase.as_bytes(), &salt_bytes,
@@ -146,6 +145,6 @@ impl<T: Read + Seek> CryptoReader<T> {
             return Err(io::Error::new(ErrorKind::Other, "Wrong passphrase"));
         }
 
-        Ok(CryptoReader { attributes, key, reader, total_len })
+        Ok(DecryptionReader { attributes, key, reader })
     }
 }
