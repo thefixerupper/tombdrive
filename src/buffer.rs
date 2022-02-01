3850b02e60a2d584a80474c6e26c2d874b4200dc
Martin Furman <68301881+thefixerupper@users.noreply.github.com>
2022-01-31 21:44:09 -0800

    Tidying up the buffer module

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,4 +1,4 @@
-// This file is part of Tombdrive
+// This file is part of Tomb Drive
 //
 // Copyright 2022 Martin Furman
 //
@@ -14,16 +14,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//!
-//! Provides [`Buffer`] that simplifies [`BufReader`]'s seeking interface.
-//!
-
 use std::io::{ self, BufReader, ErrorKind, Read, Seek, SeekFrom };
 
-///
-/// A thin shim on top of [`BufReader`] that preserves buffer when seeking
-/// via [`Seek`] functions.
-///
+// ============== //
+//     BUFFER     //
+// ============== //
+
+/// [`Buffer`] is a thin shim on top of [`BufReader`] which provides
+/// seeking with better preservation of the internal buffer.
 #[derive(Debug)]
 pub struct Buffer<T: Read + Seek> {
     cursor: usize,
@@ -32,20 +30,17 @@ pub struct Buffer<T: Read + Seek> {
 }
 
 impl<T: Read + Seek> Buffer<T> {
-    ///
-    /// Return the length of the underlying stream (as last checked).
-    ///
     pub fn len(&self) -> usize {
         self.len
     }
 
-    ///
     /// Recalculate the length of the stream that the `Buffer` operates on.
-    ///
+    /// This clears the internal buffer.
     pub fn reset(&mut self) -> io::Result<usize> {
         let current = self.reader.stream_position()?;
-        let len = self.reader.seek(SeekFrom::End(0))? as usize;
-        if len > i64::MAX as usize {
+        let len: usize = self.reader.seek(SeekFrom::End(0))?
+                                    .try_into().unwrap();
+        if len > i64::MAX.try_into().unwrap() {
             return Err(io::Error::new(ErrorKind::Other, "File too large"));
         }
         self.len = len;
@@ -53,32 +48,20 @@ impl<T: Read + Seek> Buffer<T> {
         Ok(self.len)
     }
 
-    ///
     /// Seek to a new cursor position, preserving the buffer if possible.
-    ///
     pub fn seek_from_start(&mut self, offset: usize) -> io::Result<()> {
-
-        if offset > i64::MAX as usize {
+        if offset > i64::MAX.try_into().unwrap() {
             return Err(io::Error::new(ErrorKind::InvalidInput,
                                       "Offset out of bounds"));
         }
 
-        let rel_offset = offset as i64 - self.cursor as i64;
+        let rel_offset = TryInto::<i64>::try_into(offset).unwrap()
+                       - TryInto::<i64>::try_into(self.cursor).unwrap();
         self.reader.seek_relative(rel_offset)?;
         self.cursor = offset;
         Ok(())
     }
 
-    ///
-    /// Create a new [`Buffer`] from the supplied `stream` with `capacity`
-    /// specified in bytes.
-    ///
-    /// The length of the `stream` is assumed not to change until
-    /// [`reset`] is called, at which point the buffered data gets
-    /// cleared.
-    ///
-    /// [`reset`]: Buffer::reset
-    ///
     pub fn with_capacity(capacity: usize, stream: T) -> io::Result<Buffer<T>> {
         let reader = BufReader::with_capacity(capacity, stream);
         let mut buffer = Buffer { cursor: 0, reader, len: 0 };
@@ -88,10 +71,6 @@ impl<T: Read + Seek> Buffer<T> {
 }
 
 impl<T: Read + Seek> Read for Buffer<T> {
-    ///
-    /// Read some data from the underlying stream. For more details see
-    /// [`Read`].
-    ///
     fn read(&mut self, buffer: &mut [u8]) -> io::Result<usize> {
         let bytes_read = self.reader.read(buffer)?;
         self.cursor += bytes_read;
